7dc8d301466e852f007ccc01540ddd8c930e5f66
AvishkarMandlik <mandlikavi121@gmail.com>
2025-01-10 13:36:50 +0530

    foreach and forin loop

---
+++ two.js
@@ -0,0 +1,24 @@
+// truthy and falsy values 
+//  falsy values  =  0, false, undefined, null, NaN,  document.all others are truthy values.
+
+
+// LOOPS
+
+// foreach loops only work on array operations like retrivin  elements
+
+// let a = [1,2,3,4,5,6,7,8];
+// a.forEach(function(value){
+//     console.log(value + 2);
+// });
+
+//  for in loops only work on Object 
+
+// let  obj  = {
+//     name : "Avishkar",
+//     age  : 22,
+//     gender : "male"
+// }
+
+// for(let key in obj) {
+//     console.log(key, ":",obj[key]);//values retrive using  object name and variable name
+// }
