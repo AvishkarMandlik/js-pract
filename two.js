493b9400291091e878fe383f09a0dfa9bdc9ca36
AvishkarMandlik <mandlikavi121@gmail.com>
2025-01-10 14:10:19 +0530

    is array or not and delete key and value in the object

--- two.js
+++ two.js
@@ -22,3 +22,21 @@
 // for(let key in obj) {
 //     console.log(key, ":",obj[key]);//values retrive using  object name and variable name
 // }
+
+
+
+// js mein array ek object ke format mai convert hota hai behind the scence
+// check array or object 
+let a = [1,2,3,4,5,6,7,8];
+console.log(Array.isArray([a]));//true
+console.log(Array.isArray({a}));//false
+
+
+const deatils = {
+    name:"Avishkar",
+    age:22,
+    gender:"male",
+    address:"Pune"
+}
+delete deatils.address, delete deatils.age; // delete object groups or any key and value
+console.log(deatils);
